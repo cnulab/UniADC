6c0142710046396e141aaf5d47fe537d6aa6adc0
Ximiao Zhang <113333517+cnulab@users.noreply.github.com>
2026-02-04 17:00:06 +0800

    Add files via upload

---
+++ diffusers/transformers/__init__.py
@@ -0,0 +1,8 @@
+from ..utils import is_torch_available
+
+
+if is_torch_available():
+    from .dual_transformer_2d import DualTransformer2DModel
+    from .t5_film_transformer import T5FilmDecoder
+    from .transformer_2d import Transformer2DModel
+    from .transformer_temporal import TransformerTemporalModel
